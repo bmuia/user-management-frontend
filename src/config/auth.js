e9694d9ef154db958c320271a16be814eb1ac174
bmuia <belammuia0@gmail.com>
2025-05-02 18:08:24 +0300

    refactor(auth): switch from JWT in localStorage to secure HttpOnly cookies

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,17 +1,57 @@
-import axios from "axios";
-import { API_URL } from "./apiConfig";
+import axios from 'axios';
 
+const api = axios.create({
+  baseURL: 'http://localhost:8000/',
+  withCredentials: true,
+});
 
-const api = () => {
-    const instance = axios.create({
-        baseURL: API_URL,
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("access")}`,
+let isRefreshing = false;
+let failedQueue = [];
 
-        },
-    });
-    return instance
-}
+const processQueue = (error, token = null) => {
+  failedQueue.forEach(prom => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+
+api.interceptors.response.use(
+  res => res,
+  async (err) => {
+    const originalRequest = err.config;
+
+    if (err.response?.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise(function (resolve, reject) {
+          failedQueue.push({ resolve, reject });
+        }).then(() => {
+          return api(originalRequest);
+        });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      try {
+        await api.post('auth/v1/login/refresh/',null,{
+            withCredentials: true
+        });
+        processQueue(null);
+        return api(originalRequest);
+      } catch (_err) {
+        processQueue(_err, null);
+        return Promise.reject(_err);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(err);
+  }
+);
 
 export default api;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,25 +1,22 @@
 import axios from 'axios';
 import { API_URL } from '../config/apiConfig';
+import api from '../config/auth';
 
 const API_BASE = `${API_URL}api/users/profile`;
 
-const authHeader = () => ({
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('access')}`,
-  },
-});
+
 
 export const getUser = async (id) => {
-  const res = await axios.get(`${API_BASE}/${id}/`, authHeader());
+  const res = await api.get(`${API_BASE}/${id}/`);
   return res.data;
 };
 
 export const updateUser = async (id, payload) => {
-  const res = await axios.put(`${API_BASE}/${id}/`, payload, authHeader());
+  const res = await api.put(`${API_BASE}/${id}/`, payload);
   return res.data;
 };
 
 export const deleteUser = async (id) => {
-  const res = await axios.delete(`${API_BASE}/${id}/`, authHeader());
+  const res = await api.delete(`${API_BASE}/${id}/`);
   return res.data;
 };
