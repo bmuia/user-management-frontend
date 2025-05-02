5f646e704ff5d17519048f8da2767fb479d2762f
bmuia <belammuia0@gmail.com>
2025-05-02 18:10:05 +0300

    refactor(auth): switch from JWT in localStorage to secure HttpOnly cookies

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+import { API_URL } from './apiConfig';
 const api = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: API_URL,
   withCredentials: true,
 });
 
